cf85fc5554446344eccda34feee87a608eedbdb5
fadekirues <fdkiruegbukpe@gmail.com>
2022-09-12 12:17:15 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,21 +12,21 @@ int main(void)
 	while (c <= 99)
 	{
 		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
+		l_d - (c % 10 + '0');
 
 		if (f_d < l_d)
 		{
 			putchar(f_d);
-	 		putchar(l_d);
+			putchar(l_d);
 
-	 		if (c != 89)
+			if (c != 89)
 			{
 				putchar(',');
-	  			putchar(' ');
+				putchar(' ');
 			}
 		}
 		c++;
-	}	
+	}
 putchar('\n');
 return (0);
 }
