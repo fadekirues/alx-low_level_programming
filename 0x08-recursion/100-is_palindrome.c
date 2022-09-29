28ef1e3e125e099a7a6379288b2e0add0cad8e43
fadekirues <fdkiruegbukpe@gmail.com>
2022-09-29 02:35:49 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -35,38 +35,20 @@ void check(char *s, int start, int end, int *flag)
 	}
 }
 
-
-
 /**
-
  * _strlen_recursion - calculates the length of a string
-
  * @s: string to be used
-
  *
-
  * Return: length of the string
-
  */
-
 int _strlen_recursion(char *s)
-
 {
-
 	int sum = 0;
 
-
-
 	if (*s != '\0')
-
 	{
-
 		sum++;
-
 		sum += _strlen_recursion(s + 1);
-
 	}
-
 	return (sum);
-
 }
