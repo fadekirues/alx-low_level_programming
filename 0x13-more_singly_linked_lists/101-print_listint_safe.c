6ccd47d1d5e8a498632a323d0c3d59d998d51421
fadekirues <fdkiruegbukpe@gmail.com>
2022-10-25 14:55:00 -0700

    print lias safely

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,73 @@
 #include "lists.h"
+#include <stdint>
+
+int print_listformat(const listint_t *head);
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
+ * print_listint_safe - prints lists with loops
+ * @head: pointer to first node
  *
- * Return: number of nodes in the list
- */
+ * Return: size of list
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	const listint_t *temp = head, *slow = head, *fast = head;
+	int i = 0;
 
-	while (head)
+	if (head == NULL)
+		exit(98);
+
+	while (slow && fast && fast->next)
 	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			slow = head;
+
+			while (slow->next != fast->next)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			while (temp != NULL)
+			{
+				if (temp == fast)
+				{
+					printf("[%p] %d\n", (void *)temp, temp->n);
+					i++;
+					printf("-> [%p] %d\n", (void *)fast->next, fast->next->n);
+					return (i);
+				}
+				i++;
+				printf("[%p] %d\n", (void *)temp, temp->n);
+				temp = temp->next;
+			}
+
 		}
 	}
+	i = print_listformat(head);
+	return (i);
+}
 
-	return (num);
+/**
+ * print_listformat - prints formatted list with address
+ * @head: pointer to head node
+ *
+ * Return: no of elements in list
+*/
+int print_listformat(const listint_t *head)
+{
+	const listint_t *temp = head;
+	int i = 0;
+
+	while (temp != NULL)
+	{
+		i++;
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		temp = temp->next;
+	}
+
+	return (i);
 }
