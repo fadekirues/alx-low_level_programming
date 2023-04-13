3f0f13a50eeb11c143d3c77c9afc346229f46212
fadekirues <fdkiruegbukpe@gmail.com>
2023-04-13 12:50:23 -0700

    103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,81 +1,78 @@
 #include "search_algos.h"
-/**
- * b_search - function that searches for a value in a sorted
- * array of integers using the Binary search algorithm
- *
- * @array: Array is a pointer to the first element of the array to search in
- * @low: Left index in the array
- * @high: right index in the array
- * @value: Value to search for in the array
- *
- * Return: The first index where value is located
- * or -1 if value is not found or if array is NULL
- */
-int b_search(int *array, size_t low, size_t high, int value)
-{
-	size_t left = low;
-	size_t right = high - 1;
-	size_t middle = 0;
-	size_t i = 0;
 
-	if (array != NULL)
+/**
+ * print_search_array - print the searched array
+ * @left: start index
+ * @right: end index
+ * @arr: array to print from
+ */
+void print_search_array(int left, int right, int *arr)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", arr[i]);
+	printf("%d\n", arr[right]);
+}
+
+/**
+ * binary_search_recurse - do binary search recursively
+ * @array: array to search in
+ * @left: start index
+ * @right: end index
+ * @value: value to search for
+ *
+ * Return: index of value if found, otherwise -1
+ */
+int binary_search_recurse(int *array, int left, int right, int value)
+{
+	int middle;
+
+	if (array == NULL)
+		return (-1);
+	if (right >= left)
 	{
-		while (left <= right)
-		{
-			middle = (left + right) / 2;
-			printf("Searching in array: ");
-			for (i = left; i < right; i++)
-			{
-				printf("%d, ", array[i]);
-			}
-			printf("%d\n", array[i]);
-			if (array[middle] < value)
-			{
-				left = middle + 1;
-			}
-			else if (array[middle] > value)
-			{
-				right = middle - 1;
-			}
-			else
-			{
-				return (middle);
-			}
-		}
+		print_search_array(left, right, array);
+		middle = (left + right) / 2;
+		if (array[middle] == value)
+			return (middle);
+		if (array[middle] > value)
+			return (binary_search_recurse(array, left, middle - 1, value));
+		if (array[middle] < value)
+			return (binary_search_recurse(array, middle + 1, right, value));
 	}
 	return (-1);
 }
 /**
- * exponential_search - Function that searches for a value in a sorted array
- * of integers using the Exponential search algorithm
+ * exponential_search - searches for a value in a sorted array of
+ * integers using the Exponential search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
  *
- * @array: Array is a pointer to the first element of the array to search in
- * @size: Number of elements in array
- * @value: Value to search for in the array
- *
- * Return: The first index where value is located
- * or -1 if value is not found or if array is NULL
+ * Return: index of found element, -1 if array is null or element is not found
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t bound = 1, min = 0;
-	int result;
+	int i = 0;
+	int end = 0;
 
-	if (array != NULL)
+	if (array == NULL)
+		return (-1);
+	if (array[0] == value)
 	{
-		while (bound < size && array[bound] < value)
-		{
-			printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
-			bound *= 2;
-		}
-		if (bound + 1 < size)
-			min = bound + 1;
-		else
-			min = size;
-		printf("Value found between indexes [%lu] and [%lu]\n", bound / 2, min - 1);
-		result = b_search(array, bound / 2,  min, value);
-		if (result != -1)
-			return (result);
+		printf("Value checked array[0] = [%d]\n", array[0]);
+		printf("Value found between indexes [0] and [0]\n");
+		printf("Searching in array: %d\n", array[0]);
+		return (0);
 	}
-	return (-1);
+	for (i = 1; array[i] <= value && i < (int)size; i = i * 2)
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	if (i >= (int)size)
+		end = (int)size - 1;
+	else
+		end = i;
+	printf("Value found between indexes [%d] and [%d]\n", i / 2, end);
+	return (binary_search_recurse(array, i / 2, end, value));
 }
